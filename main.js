312c9af75b1cf59f1aff39e4d9b3b9f0d8b3aa99
bechir <bechir@accessnow.org>
2016-02-03 14:27:02 +0100

    minimal functional

--- main.js
+++ main.js
@@ -12,23 +12,33 @@ app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-var User = function(id, socket, name){
+var User = function(id, socket, name, score){
     this.id = id;
     this.socket = socket;
     this.name = name;
+    this.score = score;
 };
 var users = [];
 var globalId = 0;
+var round;
+var start = function(){
+    var words = ['alpha','beta','zeta'];
+    round = setInterval(suggestWord, 5000, words);
+};
+var suggestWord = function(wordsTab){
+    var word = wordsTab[Math.floor(Math.random() * wordsTab.length)];
+    io.emit('new game', word, users);
+};
 io.on('connection', function (socket) {
     var id = ++globalId;
     var currentUser;
     console.log('new user connected, id : ' + id);
     socket.emit('ask name');
     socket.on('answer name', function(name){
-        currentUser = new User(id, socket.id, name);
+        currentUser = new User(id, socket.id, name, 0);
         console.log('user ' + id + ' is now known as ' + name);
         console.log((users.push(currentUser)) + ' regitred users');
-        socket.emit('user registred', name);
+        socket.emit('user registred', name, id);
     });
     socket.on('ready', function(){
         console.log(currentUser.name + ' is now Ready');
@@ -39,8 +49,23 @@ io.on('connection', function (socket) {
             var left = users.splice(index, 1)[0];
         console.log((left ? left.name : 'user : ' + id) + ' disconnected');
     });
+    socket.on('wins', function(id, char, score){
+        var i = users.map(function(e) { return e.id; }).indexOf(id);
+        users[i].score = score;
+        io.emit('winner', id, char, score);
+        //start();
+    });
+    socket.on('typed', function(id, char, score){
+        var i = users.map(function(e) { return e.id; }).indexOf(id);
+        users[i].score = score;
+        io.emit('type', id, char, score);
+    });
+    socket.on('loses', function(id){
+        socket.emit('the loser');
+        io.emit('loser', id);
+    });
 });
-
+start();
 http.listen(8081, function () {
     console.log('Server and Socket are Ready, both listening on *:8081');
 });
--- public/js/app.js
+++ public/js/app.js
@@ -9,13 +9,15 @@ socket.on('ask name', function () {
     var ask = appbox.append('div').attr('id', 'ask-name');
     ask.append('p').text('Please provide a nickname');
     ask.append('input').attr('type', 'text').attr('id', 'name-answer');
-    ask.append('button').text('start');
-    ask.on('submit', function () {
-        socket.emit('answer name', document.getElementById('name-answer').value);
-        return false;
+    document.getElementById('name-answer').focus();
+    ask.on('keypress', function () {
+        if (d3.event.keyCode === 13)
+            socket.emit('answer name', document.getElementById('name-answer').value);
+        else
+            return false;
     });
 });
-socket.on('user registred', function (name) {
+socket.on('user registred', function (name, id) {
     appbox.select('#ask-name').remove();
     var welcome = appbox.append('h1').text('Hello ' + name)
             .transition()
@@ -24,10 +26,92 @@ socket.on('user registred', function (name) {
             .style('opacity', 0)
             .each('end', function () {
                 d3.select(this).remove();
-                ready();
+                ready(id);
             });
 });
-function ready(){
+var arc = d3.svg.arc()
+        .innerRadius(1000 / 6.5 - 60)
+        .outerRadius(1000 / 6.5 - 5)
+        .startAngle(0);
+arc2 = arc.endAngle(Math.PI * 2);
+var svg = appbox.append('svg');
+function ready(id) {
+    var totalScore = 0;
     isReady = true;
     socket.emit('ready');
+    socket.on('new game', function (word, ids) {
+        svg.selectAll("path").remove();
+        var clock = svg.append("path")
+                .attr("class", "path path--background")
+                .attr("d", arc);
+        clock.transition().duration(5000).attr("d", arc2);
+        var score = totalScore;
+        appbox.select('#game').remove();
+        var game = appbox.append('div').attr('id', 'game');
+        game.append('h2').attr('id', 'to-guess').text(word);
+        var input = game.append('input').attr('type', 'text').attr('id', 'answer');
+        document.getElementById('answer').focus();
+        var players = game.append('dl');
+        var player = players.selectAll('.players')
+                .data(ids).enter()
+                .append('dt')
+                .attr('id', function (d) {
+                    return 'player' + d.id;
+                })
+                .text(function (d) {
+                    return d.name + ' : ';
+                });
+        player.append('dd')
+                .attr('id', function (d) {
+                    return 'score' + d.id;
+                })
+                .text(function (d) {
+                    return d.score;
+                });
+        player.append('dd')
+                .attr('id', function (d) {
+                    return 'tyspo' + d.id;
+                });
+        console.log(ids);
+        game.on('keypress', function () {
+            var next = (word.charAt(0));
+            var char = String.fromCharCode(d3.event.keyCode);
+            if (char === next) {
+                score += 10;
+                word = word.substr(1);
+                if (word.length) {
+                    socket.emit('typed', id, char, score);
+                } else {
+                    score += word.length * 10;
+                    socket.emit('wins', id, char, score);
+                    totalScore = score;
+                }
+            } else {
+                socket.emit('loses', id);
+                totalScore = score;
+            }
+        });
+        socket.on('winner', function (id, char, score) {
+            game.select('#tyspo' + id).style('color', 'green')
+                    .text(function (d) {
+                        return d3.select(this).text() + char;
+                    });
+            game.select('#score' + id).text(score);
+            game.on('keypress', null);
+            input.remove();
+        });
+        socket.on('type', function (id, char, score) {
+            game.select('#tyspo' + id).text(function (d) {
+                return d3.select(this).text() + char;
+            });
+            game.select('#score' + id).text(score);
+        });
+        socket.on('loser', function (id) {
+            game.select('#player' + id).style('color', 'red');
+        });
+        socket.on('the loser', function () {
+            game.on('keypress', null);
+            input.remove();
+        });
+    });
 }
